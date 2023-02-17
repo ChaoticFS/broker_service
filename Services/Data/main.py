07ca80bcf0bf2a808cda54e7dee443f7e86cdc44
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-02-17 01:04:24 +0100

    new library, functional thumbnail, exception fixed

--- Services/Data/main.py
+++ Services/Data/main.py
@@ -3,19 +3,19 @@ import psycopg2 as pg
 from datetime import datetime
 import requests
 import json
-from os import path
+from pathlib import Path
 
-app = FastAPI()
+#app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Server": "Ready"}
+#@app.get("/")
+#def root():
+#    return {"Server": "Ready"}
 
 def db_connection(func):
     def with_connection(*args, **kwargs):
         settings = {}
-        with open('settings.json') as json:
-            settings = json.load(json)
+        with open("Services\Data\settings.json", "r") as f:
+            settings = json.load(f)
 
         conn = pg.connect(
             host=f"{settings['host']}",
@@ -52,20 +52,16 @@ def update_mapping(**kwargs):
     items = []
     errors = [] # Connect this to logging after filling
 
-    for x in result: # Item Id, Members?, Trade limit, High Alch, Name
-        try:
-            limit = str(x["limit"])
-        except:
-            limit = "None"
-        try:
-            highalch = str(x["highalch"])
-        except:
-            highalch = "None"
+    for item in result: # Item Id, Members?, Trade limit, High Alch, Name
+        if not "limit" in item:
+            item["limit"] = 0
+        if not "highalch" in item:
+            item["highalch"] = 0
 
         try:
-            items.append((x["id"], x["members"], limit, highalch, x["name"]))
+            items.append((item["id"], item["members"], item["limit"], item["highalch"], item["name"]))
         except KeyError:
-            error = (x["id"], x["name"])
+            error = (item["id"], item["name"])
             errors.append(error)
 
     # Remove bad data from known ids
@@ -82,20 +78,21 @@ def update_mapping(**kwargs):
     
     values_string = ",".join(cur.mogrify("(%s, %s, %s, %s, %s)", x) for x in items)
 
-def get_item_thumbnail(item_id): # Would recommend using a less janky storage method than a folder
-    thumbnail_path = f"./Thumbnails/{item_id}.png"
+def get_item_thumbnail(item_id): # Consider switching to just referencing the runelite host instead of selfhosting it, mostly did it for stability
+    dir_path = Path(__file__).parents[0]
+    img_path = dir_path / "Thumbnails" / f"{str(item_id)}.png"
 
-    if not path.exists(thumbnail_path):
+    if not Path.exists(img_path):
         url = f"https://static.runelite.net/cache/item/icon/{item_id}.png"
         response = requests.get(url)
-
+        
         if response.status_code == 200:
-            with open(f"thumbnails/{item_id}.png", "wb") as f:
+            with open(img_path, "wb") as f:
                 f.write(response.content)
 
-        return thumbnail_path
+        return img_path
 
-    return thumbnail_path
+    return img_path
 
 @db_connection
 def store_5min(**kwargs):
